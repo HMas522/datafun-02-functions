050a68367f4683734f4a700b284aee06087150c8
Hayley Massey <Hmassey12@outlook.com>
2023-05-19 13:22:07 -0500

    version 5

--- user_math.HayleyM.py/Project 2 -Math.py
+++ user_math.HayleyM.py/Project 2 -Math.py
@@ -16,6 +16,8 @@ import doctest
 from util_datafun_logger import setup_logger
 logger, logname = setup_logger(__file__)
 
+"""In the game Monster Hunter, you are given a house to change equipment and re-stock items. How big is your video game house?"""
+
 length= int(input("Enter the length of the video game house in meters: "))
 width = int(input("Enter the width of the video game house in meters: "))
 
@@ -24,6 +26,18 @@ area = length * width
 print("The total area of your video game house is", area, "square meters.")
 
 logger.info(f"The total area of your video game house is {area}.")
+
+"""Another weapon type in Monster Hunter, is the Great Sword. This is slow hits, but large damage. 
+Please input the number of hits and the damanage dealt to get the total damange to the monster """
+
+#list of hits
+arr = [1, 100, 2, 100,]
+#calculate the product
+product = math.prod(arr)
+print(product)
+
+logger.info(f"The total damage done to the monster is {product}.")
+
 
 # Read log file and print it to the terminal
 with open(logname, 'r') as file_wrapper:
