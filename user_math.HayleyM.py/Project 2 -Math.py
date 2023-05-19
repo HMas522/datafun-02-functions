ce742961b138f7631ea0bcc0bc332169b768d540
Hayley Massey <Hmassey12@outlook.com>
2023-05-19 13:37:23 -0500

    version 6

--- user_math.HayleyM.py/Project 2 -Math.py
+++ user_math.HayleyM.py/Project 2 -Math.py
@@ -38,6 +38,12 @@ print(product)
 
 logger.info(f"The total damage done to the monster is {product}.")
 
+"""Let's say the game input can only understand radians, not degrees. If your character moves 1 radian, how many degrees does your character turn?"""
+#moving the slightly tells the charcter to move. 
+print(math.derees(1.1))
+
+logger.info(f"The character moves {math.degrees(1.1)} degrees).")
+
 
 # Read log file and print it to the terminal
 with open(logname, 'r') as file_wrapper:
